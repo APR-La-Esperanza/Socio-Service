c25034bf0e389e4d6ec7a6b41bb74ff457ad8f90
renatoR4amos <re.ramoss@duocuc.cl>
2026-06-03 18:03:40 -0400

    guardado 3 (configuracion)

---
+++ src/main/java/com/apr/Socio_Service/SocioServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apr.Socio_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocioServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apr/Socio_Service/SocioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apr.Socio_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
